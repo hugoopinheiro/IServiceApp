cd8b3f1901e8dd1e1bd962b2372f04ef9a37037f
Hugo Steverson Pinheiro Bizerra <128258769+hugoopinheiro@users.noreply.github.com>
2024-10-31 19:00:21 -0300

    Delete Iservice/src/main/java/br/com/crisgo/iservice/DTO/RequestProduct.java

--- Iservice/src/main/java/br/com/crisgo/iservice/DTO/RequestProduct.java
+++
@@ -1,6 +0,0 @@
-package br.com.crisgo.iservice.DTO;
-
-import java.time.LocalDateTime;
-
-public record RequestProduct(String productName, LocalDateTime createdAt, String description, Double price, String category) {
-}
