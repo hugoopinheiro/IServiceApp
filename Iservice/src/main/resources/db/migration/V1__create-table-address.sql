eb60f9898ac961c2b8ebd9ff17c3eed8efaecbcb
Hugo Steverson Pinheiro Bizerra <hugosteverson11@gmail.com>
2025-02-01 19:34:18 -0300

    updated the structure database of seller and address.

--- Iservice/src/main/resources/db/migration/V1__create-table-address.sql
+++ Iservice/src/main/resources/db/migration/V1__create-table-address.sql
@@ -1,8 +1,8 @@
 CREATE TABLE address (
     address_id SERIAL PRIMARY KEY,
-    street VARCHAR(200) NOT NULL,
-    cep VARCHAR(10) NOT NULL,
+    street VARCHAR(200) ,
+    cep VARCHAR(10) ,
     complement VARCHAR(200),
-    state VARCHAR(50) NOT NULL,
-    house_number VARCHAR(10) NOT NULL
+    state VARCHAR(50) ,
+    house_number VARCHAR(10)
 );
--- Iservice/src/main/resources/db/migration/V3__create-table-users.sql
+++ Iservice/src/main/resources/db/migration/V3__create-table-users.sql
@@ -1,12 +1,14 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     address_id INTEGER,
-    name VARCHAR(100) NOT NULL,
-    user_name VARCHAR(100) NOT NULL, -- "userName" field in the Java entity
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    contact VARCHAR(11) NOT NULL,
+    name VARCHAR(100),
+    user_name VARCHAR(100) , -- "userName" field in the Java entity
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(100),
+    contact VARCHAR(11),
     active BOOLEAN DEFAULT TRUE,
+    permissions VARCHAR(15),
+    role VARCHAR(15),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_address FOREIGN KEY (address_id) REFERENCES address(address_id)
 );
