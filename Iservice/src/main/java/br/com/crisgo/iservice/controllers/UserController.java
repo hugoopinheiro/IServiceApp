a3c36a669b95fcb33a5b513673683a84a92be06e
Hugo Steverson Pinheiro Bizerra <hugosteverson11@gmail.com>
2025-02-01 19:32:54 -0300

    removed some things and created the method createSeller().

--- Iservice/src/main/java/br/com/crisgo/iservice/controllers/UserController.java
+++ Iservice/src/main/java/br/com/crisgo/iservice/controllers/UserController.java
@@ -1,14 +1,12 @@
 package br.com.crisgo.iservice.controllers;
 
+import br.com.crisgo.iservice.DTO.request.RequestSellerDTO;
 import br.com.crisgo.iservice.DTO.response.ResponseUserDTO;
 import br.com.crisgo.iservice.DTO.request.RequestUserDTO;
-import br.com.crisgo.iservice.config.SecurityConfig;
+import br.com.crisgo.iservice.services.SellerService;
 import br.com.crisgo.iservice.services.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,30 +15,21 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.hateoas.EntityModel;
 
 @RestController
-@RequestMapping(value = "/user", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+@RequestMapping(value = "/api/v1/user", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 @Tag(name = "usuario", description = "Controlador para salvar operações e editar dados do usuario")
-@SecurityRequirement(name = SecurityConfig.SECURITY)
 public class UserController {
+    private final UserService userService;
+    private final SellerService sellerService;
 
-    @Autowired
-    private UserService userService;
+    public UserController(UserService userService, SellerService sellerService) {
+        this.userService = userService;
+        this.sellerService = sellerService;
+    }
 
-    @GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    @Operation(summary = "Busca dados de um usuario especifico", description = "Metodo para busca dados de um usuario")
-    @ApiResponse(responseCode = "200", description = "usuario encontrado com sucesso")
-    @ApiResponse(responseCode = "400", description = "usuario nao existe")
-    @ApiResponse(responseCode = "500", description = "Erro no servidor")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<EntityModel<ResponseUserDTO>> getUser(@PathVariable Long id) {
         ResponseUserDTO userDTO = userService.findById(id);
         EntityModel<ResponseUserDTO> resource = EntityModel.of(userDTO);
-        return ResponseEntity.ok(resource);
-    }
-
-    @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-    public ResponseEntity<EntityModel<ResponseUserDTO>> createUser(@RequestBody @Validated RequestUserDTO requestUserDTO) {
-        ResponseUserDTO createdUser = userService.save(requestUserDTO);
-        EntityModel<ResponseUserDTO> resource = EntityModel.of(createdUser);
         return ResponseEntity.ok(resource);
     }
 
@@ -50,11 +39,17 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping(value = "/{id}", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
-            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @PutMapping(value = "/{id}")
     public ResponseEntity<EntityModel<ResponseUserDTO>> updateUser(@PathVariable Long id, @RequestBody @Validated RequestUserDTO requestUserDTODetails) {
         ResponseUserDTO updatedUser = userService.updateUser(id, requestUserDTODetails);
         EntityModel<ResponseUserDTO> resource = EntityModel.of(updatedUser);
         return ResponseEntity.ok(resource);
     }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<ResponseUserDTO> createSeller(@PathVariable Long id, @RequestBody @Valid RequestSellerDTO requestSellerDTO) {
+        ResponseUserDTO response = userService.createSeller(id, requestSellerDTO);
+        return ResponseEntity.ok(response);
+    }
+
 }
